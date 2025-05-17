3f9c25647e87a3c3d9447d6552b5c89fcd8c71fe
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-17 15:21:52 +0100

    ba ni son tsayayya!

--- count_files.sh
+++ count_files.sh
@@ -3,10 +3,8 @@
 echo "Enter the name of the directory:"
 read directory
 #Calcul le nombre de fichier à traver un argument
-if [ -d "$directory" ]; then
-  count=$(ls -p "$directory" | grep -v / | wc -l)
+
+  count=$(ls -p "$directory" | wc -l)
   echo "There are $count files in the directory '$directory'."
-else
-  echo "Directory '$directory' does not exist."
-fi
+
 
