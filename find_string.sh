0d6ccb281775ef96d50586129116fd6b2b5f3d90
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-17 18:38:02 +0100

    ?

--- find_string.sh
+++ find_string.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-
 # Lire le nom du fichier et le nom à rechercher
-echo "Entrez le nom du fichier :"
 read filename
-
-echo "Entrez le nom (ou la chaîne) à rechercher :"
 read name
-
 # Vérifie si la chaîne existe dans le fichier
 if grep -q "$name" "$filename"; then
     echo "La chaîne '$name' a été trouvée dans '$filename'."
