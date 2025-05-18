2ec9cd22a99fb11f4ffee1c66341756df0045cf6
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-18 15:45:23 +0100

    m

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 # Ce script affiche tous les arguments, un par ligne
-
 # Vérifie s'il y a au moins un argument
 if [ $# -eq 0 ]; then
   echo "Aucun argument fourni."
   exit 1
 fi
-
 # Affiche chaque argument sur une ligne
 for arg in "$@"; do
   echo "$arg"
