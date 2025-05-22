9619513f685f637639e7811ee6468c609ae3cd68
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-22 02:20:00 +0100

    calculatrice bash

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Demande les nombres et l'opérateur
-
 read a
-
 read b
-
 read op
 # Calcule selon l'opérateur
 if [ "$op" = "+" ]; then
@@ -14,12 +11,6 @@ elif [ "$op" = "-" ]; then
 elif [ "$op" = "*" ]; then
   echo "Résultat : $((a * b))"
 elif [ "$op" = "/" ]; then
-  if [ "$b" -eq 0 ]; then
-    echo "Erreur : division par zéro "
-  else
     echo "Résultat : $((a / b))"
-  fi
-else
-  echo "Opérateur non reconnu "
 fi
 
