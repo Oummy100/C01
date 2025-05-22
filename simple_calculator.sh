9c88c6b51b1dff2e7381ad96b75c0128aa9757a2
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-22 02:00:51 +0100

    calculatrice bash

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-
 # Demande les nombres et l'opérateur
 echo "Entrez le premier nombre :"
 read a
-
 echo "Entrez le deuxième nombre :"
 read b
-
 echo "Entrez l'opérateur (+, -, *, /) :"
 read op
-
 # Calcule selon l'opérateur
 if [ "$op" = "+" ]; then
   echo "Résultat : $((a + b))"
